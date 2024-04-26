663b16260ccda6ad515d436546aea1c21292bc86
JothishJJ <jothishjj11@gmail.com>
2024-04-26 23:06:57 +0530

    Add the handler for India Page in Backend

---
+++ server/router/handlers.go
@@ -0,0 +1,35 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/JothishJJ/thedailybrief/crawler"
+	"github.com/gin-gonic/gin"
+)
+
+func HomeHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Hello World!",
+	})
+}
+
+func WorldHandler(c *gin.Context) {
+
+	crawlerJobs := crawler.Websites{
+		"https://timesofindia.indiatimes.com/rssfeeds/296589292.cms",
+	}
+
+	allDatas := crawler.GetAllData(crawlerJobs)
+
+	c.JSON(http.StatusOK, allDatas)
+}
+
+func IndiaHandler(c *gin.Context) {
+	crawlerJobs := crawler.Websites{
+		"https://timesofindia.indiatimes.com/rssfeeds/-2128936835.cms",
+	}
+
+	allDatas := crawler.GetAllData(crawlerJobs)
+
+	c.JSON(http.StatusOK, allDatas)
+}
