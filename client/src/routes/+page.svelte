2912757a362dd3a767af16ef7482bab652b7797b
JothishJJ <jothishjj11@gmail.com>
2024-04-26 10:03:44 +0530

    Add cors

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,1 +1,16 @@
+<script>
+	import { onMount } from 'svelte';
+
+	/** @type {any | undefined} */
+	let dataObj;
+	onMount(async () => {
+		const response = await fetch('http://localhost:8080');
+		const data = await response.json();
+		dataObj = data;
+	});
+</script>
+
 <h1>Hello World</h1>
+{#if dataObj}
+	<div>{dataObj.message}</div>
+{/if}
--- server/main.go
+++ server/main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:5173"}
+
+	r.Use(cors.New(config))
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Hello World!",
